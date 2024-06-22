6bc12d1a5beca3927cfa9c99775904c3af0699fd
NibakureJosee <joseenibakure@gmail.com>
2024-06-23 01:17:16 +0200

    commit

---
+++ src/main/java/rw/ne/laptopdistribution/LaptopDistributionApplication.java
@@ -0,0 +1,13 @@
+package rw.ne.laptopdistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopDistributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopDistributionApplication.class, args);
+	}
+
+}
