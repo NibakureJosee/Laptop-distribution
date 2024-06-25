280d9eb6b7b1b4e311a9f4a813ff1e5762d56926
NibakureJosee <joseenibakure@gmail.com>
2024-06-25 13:39:57 +0200

    commit

--- src/main/java/rw/ne/laptopdistribution/serviceImpls/EmailServiceImpl.java
+++ src/main/java/rw/ne/laptopdistribution/serviceImpls/EmailServiceImpl.java
@@ -1,9 +1,10 @@
-package rw.ne.laptopdistribution.services;
+package rw.ne.laptopdistribution.serviceImpls;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import rw.ne.laptopdistribution.services.EmailService;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -13,7 +14,9 @@ public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    @Autowired
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public EmailServiceImpl(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
@@ -23,12 +26,13 @@ public class EmailServiceImpl implements EmailService {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
         try {
+            helper.setFrom(fromEmail);
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setText(body, true); // Enable HTML content if needed
+            helper.setText(body);
             javaMailSender.send(message);
         } catch (MessagingException e) {
-            e.printStackTrace(); // Handle the exception according to your application's error handling strategy
+            e.printStackTrace(); // Handle the exception as per your application's error handling strategy
         }
     }
 }
